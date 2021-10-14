6dafe05c1054bfe0f9fc3d2de7893046675882a9
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-10-14 11:06:29 +0200

    Subida de ejercicios  del Tema 2

---
+++ Tema02/Tema02Ejercicio07.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicios Tema 02
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Tema02Ejercicio07 {
+  public static void main(String[] args) {
+    char puerta = 'p';
+    String sillon = "sillon";
+    char sofa = 's';
+    String mesa = "mesa";
+    
+    System.out.println("" + puerta + sillon + sofa + mesa);
+  }
+}
---
+++ Tema02/Tema02Ejercicio08.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicios Tema 02
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Tema02Ejercicio08 {
+  public static void main(String[] args) {
+    char p = 'p';
+    char u = 'u';
+    char n = 'n';
+    char t = 't';
+    char o = 'o';
+    String cadena = "" + p + u + n + t + o;
+    
+    System.out.println(cadena);
+  }
+}
