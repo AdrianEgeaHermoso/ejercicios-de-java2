3be7892fc6ce598488335bfaf9c0e79e9bc20a52
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-10-21 13:26:14 +0200

    Subida de ejercicios 6 y 8 del Tema 04

---
+++ Tema04/Tema04Ejercicio06.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Tema04Ejercicio06 {
+  public static void main(String[] args) {  
+     
+    System.out.println("Programa para calcular el tiempo que tardará un objeto en caer desde una altura determinada: ");   
+    System.out.print("Por favor, introduzca la altura en metros: ");
+    
+    double altura = Double.parseDouble(System.console().readLine());
+    
+    double gravedad = 9.81;
+    double tiempo = Math.sqrt(2*altura/gravedad);
+
+    System.out.printf("El objeto que dejemos caer, tardará %.2f segundos", tiempo);
+
+  }
+}
---
+++ Tema04/Tema04Ejercicio08.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Tema04Ejercicio08 {
+  public static void main(String[] args) {  
+     
+    System.out.println("Programa que calcula la media de tres notas y te lo notifique con insuficiente, suficiente, bien, notable o sobresaliente: ");   
+    System.out.print("Por favor, introduzca la primera nota: ");
+    
+    double primeraNota = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("Por favor, introduzca la segunda nota: ");
+    
+    double segundaNota = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("Por favor, introduzca la tercera nota: ");
+    
+    double terceraNota = Double.parseDouble(System.console().readLine());
+    
+    double notaMedia = (primeraNota + segundaNota + terceraNota) / 3;
+    
+    if (notaMedia < 5){
+      System.out.print("Su nota media es insuficiente");
+      }else if ((notaMedia >= 5) && (notaMedia < 6)){
+        System.out.print("Su nota media es suficiente");
+        }else if ((notaMedia >= 6) && (notaMedia < 7)){
+          System.out.print("Su nota media es bien");
+          }else if ((notaMedia >= 7) && (notaMedia < 9)){
+            System.out.print("Su nota media es notable");
+            }else if ((notaMedia >= 9) && (notaMedia <= 10)){
+              System.out.print("Su nota media es sobresaliente");
+              }else{
+                System.out.print("Datos introducidos incorrectos");// por si alguien introduce números disparatados
+                }
+
+  }
+}
