6e81f9e2f40d7db3348d63ec91ab6c506514d9ed
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-10-25 09:27:41 +0200

    Subida de ejercicios con mejoras corregidas  del Tema 04

--- Tema04/Tema04Ejercicio01.java
+++ Tema04/Tema04Ejercicio01.java
@@ -9,7 +9,9 @@ public class Tema04Ejercicio01 {
     System.out.println("Programa para introducir un día de la semana y te dice que asignatura hay a primera hora: ");   
     System.out.print("Por favor, introduzca el dia de la semana: ");
     
-    String diaSemana = System.console().readLine();
+    String diaSemana = System.console().readLine().toLowerCase();// con el lowercase pasa a minuscula el dato
+    
+    //diaSemana = diaSemana.toLowerCase();
     
     
     switch (diaSemana) {
