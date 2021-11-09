90d1e5b110978e2aaf9979e6fc05b19c20dda6f4
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-11-09 09:52:10 +0100

    Subida de ejercicios del Tema 05

--- Tema05/Tema05Ejercicio02.java
+++ Tema05/Tema05Ejercicio02.java
@@ -5,7 +5,7 @@
  */
 public class Tema05Ejercicio02 {
   public static void main(String[] args) {
-    System.out.println("Programa que muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle: ");
+    System.out.println("Programa que muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while: ");
     
     int i = 0;
     
---
+++ Tema05/Tema05Ejercicio03.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Tema05Ejercicio03 {
+  public static void main(String[] args) {
+    System.out.println("Programa que muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle do while: ");
+    
+    int i = 0;
+    
+    System.out.println(0);
+    
+    do{
+      System.out.println(i += 5);
+    }while (i < 100);
+    
+  }
+}
