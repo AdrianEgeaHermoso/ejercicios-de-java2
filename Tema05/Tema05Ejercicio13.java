a03f0576b3e29c81d1a4831bc2c58426079b2f29
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-11-11 11:14:26 +0100

    Subida de ejercicio 13 sin pulir del Tema 05

---
+++ Tema05/Tema05Ejercicio13.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Tema05Ejercicio13 {
+  public static void main(String[] args) {
+    System.out.println(" Escribe un programa que lea una lista de diez números y determine cuántos son positivos, y cuántos son negativos.: ");
+    
+    int n = 0;
+    int numeros = 0;
+    
+    do{
+      System.out.println("Introduzca números para saber si es positivo o negativo: ");
+      n = Integer.parseInt(System.console().readLine());
+      if (n>= 0){
+        System.out.println("Es positivo");
+      }else{System.out.println("Es negativo");}
+      numeros ++;
+    }while(numeros <= 10);
+  
+  }
+}
