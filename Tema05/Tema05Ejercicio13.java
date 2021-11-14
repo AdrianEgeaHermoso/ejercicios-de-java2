bc72db2c6e7f6eecd40e7260b9b30144e84a3b6c
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-11-14 19:13:20 +0100

    Subida de ejercicio 13 del Tema 05

--- Tema05/Tema05Ejercicio13.java
+++ Tema05/Tema05Ejercicio13.java
@@ -8,16 +8,27 @@ public class Tema05Ejercicio13 {
     System.out.println(" Escribe un programa que lea una lista de diez números y determine cuántos son positivos, y cuántos son negativos.: ");
     
     int n = 0;
-    int numeros = 0;
+    int positivos = 0;
+    int negativos = 0;
     
-    do{
-      System.out.println("Introduzca números para saber si es positivo o negativo: ");
+    System.out.println("Introduce 10 numeros para saber si son positivos o negativos");
+    
+    for (int i = 0; i < 10; i++) {
       n = Integer.parseInt(System.console().readLine());
-      if (n>= 0){
-        System.out.println("Es positivo");
-      }else{System.out.println("Es negativo");}
-      numeros ++;
-    }while(numeros <= 10);
+      if (n >= 0){positivos ++;
+      }else{negativos ++;};
+        }
+    
+    System.out.println("Ha introducido " + positivos + " numeros positivos y " + negativos + " negativos");
+    
+    //do{
+      //System.out.println("Introduzca números para saber si es positivo o negativo: ");
+      //n = Integer.parseInt(System.console().readLine());
+      //if (n>= 0){
+        //System.out.println("Es positivo");
+      //}else{System.out.println("Es negativo");}
+      //numeros ++;
+    //}while(numeros <= 10);
   
   }
 }
