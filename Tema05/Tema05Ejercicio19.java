8d5eeddc5fe6ce592b59f5f5de8fe2507b8a0202
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-11-16 10:12:21 +0100

    Subida de ejercicios del Tema 05

---
+++ Tema05/Tema05Ejercicio19.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Tema05Ejercicio19 {
+  public static void main(String[] args) {
+    System.out.println("Realiza un programa que pinte una pirámide por pantalla. La altura se debe pedir por teclado. El carácter con el que se pinta la pirámide también se debe pedir por teclado.");
+    
+    
+    System.out.print("Introduzca la altura de la piramide: ");
+    int altura = Integer.parseInt(System.console().readLine());
+    
+    System.out.print("Introduzca el caracter con el que quiere pintar la piramide: ");
+    String c = System.console().readLine();
+    
+    int base = 1;
+    int longitudDeLinea = 1;
+    int espacios = altura - 1;
+    
+    while (base <= altura) {
+      
+      // inserta espacios
+      for (int i = 1; i <= espacios; i++) {
+        System.out.print(" ");
+      }
+
+      // pinta la línea
+      for (int i = 1; i <= longitudDeLinea; i++) {
+        System.out.print(c);
+      }
+
+      System.out.println();
+
+      base++;
+      espacios--;
+      longitudDeLinea += 2;
+    }
+  
+  }
+}
