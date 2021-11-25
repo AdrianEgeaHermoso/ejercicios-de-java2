71a67eeb2f5ab9b554a6450953688778f5b3afb8
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-11-25 10:31:24 +0100

    Subida de ejercicios del Tema 06

--- Tema06/Tema06Ejercicio14.java
+++ Tema06/Tema06Ejercicio14.java
@@ -3,7 +3,7 @@
  *
  *  @author Adrián Egea Hermoso
  */
-public class Tema06Ejercicio06 {
+public class Tema06Ejercicio14 {
   public static void main(String[] args) {
     System.out.println("Escribe un programa que piense un número al azar entre 0 y 100. El usuario debe adivinarlo y tiene para ello 5 oportunidades. Después de cada intento fallido, el programa dirá cuántas oportunidades quedan y si el número introducido es menor o mayor que el número secreto.");
     
