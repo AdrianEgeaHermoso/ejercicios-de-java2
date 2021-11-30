d53c4d094322ef7941fd380945fce603a121c183
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2021-11-30 08:52:56 +0100

    Subida de ejercicios del Tema 07

---
+++ Tema07/Tema07Ejercicio01.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * 
+ * @author Adrián Egea Hermoso
+ */
+public class Tema07Ejercicio01 {
+  public static void main(String[] args) {
+        
+    
+    int[] num = new int[12];
+    
+            
+    num[0] = 39;
+    num[1] = -2;
+    num[4] = 0;
+    num[6] = 14;
+    num[8] = 5;
+    num[9] = 120;
+    
+    for (int i = 0; i < num.length; i++) {
+      System.out.println(num[i]);
+    }
+    
+    
+  }
+}
---
+++ Tema07/Tema07Ejercicio02.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * 
+ * @author Adrián Egea Hermoso
+ */
+public class Tema07Ejercicio02 {
+  public static void main(String[] args) {
+        
+    
+    char[] simbolo = new char[10];
+    
+            
+    simbolo[0] = 'a';
+    simbolo[1] = 'x';
+    simbolo[4] = '@';
+    simbolo[7] = '+';
+    simbolo[8] = 'Q';
+    
+    for (int i = 0; i < simbolo.length; i++) {
+      System.out.println(simbolo[i]);
+    }
+    
+    
+  }
+}
