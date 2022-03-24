969798d40afb8511dbf0322c17df147983182043
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2022-03-24 10:23:59 +0100

    modificación del ejercicio 8

--- Tema10/Ejercicio08.java
+++ Tema10/Ejercicio08.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Ejercicio08 {
     public static void main(String[] args) {
         
-        ArrayList<Baraja> cartas = new ArrayList<Baraja>();
+        ArrayList<Carta> cartas = new ArrayList<Carta>();
 
-        
+
     }
 }
