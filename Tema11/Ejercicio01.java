3dab0df7bc2835894473f5989a7f9bdf3671a34f
AdrianEgeaHermoso <egeaadrian14@gmail.com>
2022-04-25 23:51:55 +0200

    ejercicios Tema 10 y 11

---
+++ Tema11/Ejercicio01.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class Ejercicio01 {
+  public static void main(String[] args) {
+    try {
+      BufferedWriter bw = new BufferedWriter(new FileWriter("primos.dat"));
+
+      for (int i = 1; i < 501; i++) {
+        if (esPrimo(i)) {
+          //bw.write(String.valueOf(i) + "\n");
+          System.out.println(String.valueOf(i) + "\n");
+        }
+      }
+      bw.close();
+
+    } catch (IOException ioe) {
+        System.out.println("Error de escritura.");
+    }
+  }
+
+
+  public static boolean esPrimo(int x) {
+    for (int i = 2; i < x; i++) {
+      if ((x % i) == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
--- Tema11/MediaFichero.java
+++ Tema11/MediaFichero.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 class MediaFichero {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         try {
             BufferedReader br = new BufferedReader(new FileReader(args[0]));
